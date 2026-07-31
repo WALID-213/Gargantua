5ed7296e03f1878285bc04ebed52ffe0c1925587
WALID-213 <sonwalid213@gmail.com>
2026-07-31 19:27:02 +0100

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,35 @@
+/* ===========================================
+   GARGANTUA
+   Space Engine
+   Version: Alpha 1.0
+=========================================== */
 
+/* ===== Canvas ===== */
+
+const canvas = document.getElementById("stars-canvas");
+const ctx = canvas.getContext("2d");
+
+/* ===== Resize ===== */
+
+function resizeCanvas() {
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+}
+
+resizeCanvas();
+
+window.addEventListener("resize", resizeCanvas);
+
+/* ===== Animation Loop ===== */
+
+function animate() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    requestAnimationFrame(animate);
+
+}
+
+animate();
