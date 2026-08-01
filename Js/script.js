cee61de690b2c5170f3b5b7c3fc7df6258994584
WALID-213 <sonwalid213@gmail.com>
2026-08-01 10:35:28 +0100

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -12,66 +12,43 @@ const ctx = canvas.getContext("2d");
 /* ===== Resize ===== */
 
 function resizeCanvas() {
-
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
 }
 
 resizeCanvas();
 
-window.addEventListener("resize", 
-     /* ===== Stars ===== */
+window.addEventListener("resize", resizeCanvas);
+
+/* ===== Stars ===== */
 
 const stars = [];
 
 const STAR_COUNT = 150;
 
 for (let i = 0; i < STAR_COUNT; i++) {
-
     stars.push({
-
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
-
         radius: Math.random() * 2 + 0.3,
-
         opacity: Math.random()
-
     });
+}
 
-}                   
 /* ===== Animation Loop ===== */
 
 function animate() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    requestAnimationFrame(animate);
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     stars.forEach(star => {
-
         ctx.beginPath();
-
-        ctx.arc(
-            star.x,
-            star.y,
-            star.radius,
-            0,
-            Math.PI * 2
-        );
-
+        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
         ctx.fillStyle = `rgba(255,255,255,${star.opacity})`;
-
         ctx.fill();
-
     });
 
     requestAnimationFrame(animate);
-
-}
-
 }
 
 animate();
